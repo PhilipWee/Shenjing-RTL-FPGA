c71c482275dbcd556c3d5a41d7aabfb5ed8c6f24
Philip Wee <philip.andrewweedewang@gmail.com>
2021-03-06 14:43:55 +0800

    Updated with additional functionality
    Added:
    
    SLP,
    CEB,
    SD,
    RSTB
    
    for sram implementation

--- Shenjing-RTL-FPGA.srcs/sim_1/new/testbench.v
+++ Shenjing-RTL-FPGA.srcs/sim_1/new/testbench.v
@@ -25,14 +25,14 @@ module testbench;
     parameter SRAM_WIDTH = 64;
     parameter ADDR_WIDTH = 6; //Why is it 7 in the original code?
 
-    reg CLK,WEB;
+    reg CLK,WEB,SLP,CEB,SD,RSTB;
     reg [SRAM_WIDTH-1:0]    D;
     reg [ADDR_WIDTH-1:0]    A;
     
     
     wire [SRAM_WIDTH-1:0]    Q;
 
-    sram_custom UUT(.CLK(CLK), .D(D), .Q(Q), .A(A),.WEB(WEB));
+    sram_custom UUT(.CLK(CLK), .D(D), .Q(Q), .A(A),.WEB(WEB),.SLP(SLP),.CEB(CEB),.SD(SD),.RSTB(RSTB));
         
     always #5 CLK = ~CLK;
     
@@ -47,6 +47,10 @@ module testbench;
         //Try writing the number 10 to address 3
         WEB <= 1;
         CLK <= 0;
+        RSTB <= 0;
+        SLP <= 0;
+        CEB <= 0;
+        SD <= 0;
         D <= 10;
         A <= 3;
         #20;
@@ -65,8 +69,66 @@ module testbench;
         A <= 3;
         #20;
         `assert(Q,10);
-        
-
+        //Try putting CEB to high and check if there is output
+        CEB <= 1;
+        #5
+        `assert(Q,0);
+        #20; 
+        //Try putting CEB to low and check if there is output
+        CEB <= 0;
+        #5;
+        `assert(Q,10);
+        #20;
+        //Try shutting down and checking if there is output
+        SD <= 1;
+        #5
+        `assert(Q,0);
+        #20
+        //Try changing the address while it has been shut down and checking if there is output
+        A <= 5;
+        #5
+        `assert(Q,0);
+        //Try turning it back on and checking that there is still no output
+        SD <= 0;
+        #5
+        `assert(Q,0);
+        //rewrite a value to the register (Address is already 5)
+        D <= 30;
+        WEB <= 1;
+        #20;
+        `assert(Q,30);
+        WEB <= 0;
+        #20
+        //Stop the chip enable and check that there is no output
+        CEB <= 1;
+        #5
+        `assert(Q,0);
+        #20
+        //Renable the chip and check there is output
+        CEB <= 0;
+        #5
+        `assert(Q,30);
+        #20
+        //Reset the chip and check that the value has been reset to 0
+        RSTB <= 1;
+        #20
+        `assert(Q,0);
+        RSTB <= 0;
+        #20
+        //Rewrite a value to test there is output
+        A <= 0;
+        #5
+        D <= 39;
+        WEB <= 1;
+        #20
+        `assert(Q,39);
+        #20
+        //Put the chip to sleep, change the data in and see if there is any output changes
+        SLP <= 1;
+        D <= 22;
+        #20
+        `assert(Q,39);
+        //End of tests
 //        $finish;
     end
 
--- Shenjing-RTL-FPGA.srcs/sources_1/new/sram_custom.v
+++ Shenjing-RTL-FPGA.srcs/sources_1/new/sram_custom.v
@@ -26,27 +26,53 @@ module sram_custom(
         Q, //Output
         A, //Address
         WEB, //Write enable
-
+        SLP, //Memory Sleep enable
+        CEB, //Memory Chip Enable (Low active)
+        SD, //Memory shut down
+        RSTB //Reset button
     );
     
     parameter SRAM_WIDTH = 64;
     parameter ADDR_WIDTH = 6; //Why is it 7 in the original code?
     
-    input   CLK;
+    input   CLK,SLP,CEB,SD,RSTB;
     input   [SRAM_WIDTH-1:0]    D;
     input   [ADDR_WIDTH-1:0]    A;
     input   WEB;
     
+    task RESET_MEMORY;
+        for(index = 0; index < SRAM_WIDTH; index = index + 1) begin
+            sram_data[index] <= 0;
+        end
+    endtask
+    
     reg [SRAM_WIDTH-1:0] sram_data[SRAM_WIDTH-1:0];
+    reg in_shut_down = 1'b0;
+    
+    integer index; //Does this create a new register?
     
     output [SRAM_WIDTH-1:0] Q;
     
-    assign Q = sram_data[A];
+    assign Q = ~CEB & ~SD ? sram_data[A] : 0;
     
-    always@(posedge CLK)
-    begin
-        if (WEB)
-            sram_data[A] = D;
+    initial begin
+        RESET_MEMORY;
+    end
+    
+    always@(posedge CLK) begin
+    if (~SD & in_shut_down)
+        in_shut_down <= 1'b0;
+    if (~SD & ~SLP) begin
+        if (WEB & ~CEB )
+            sram_data[A] <= D;
+        end
+        if (RSTB) begin
+            RESET_MEMORY;
+        end
+    else if (SD & ~in_shut_down) begin
+            RESET_MEMORY;
+            in_shut_down <= 1'b1;
+        end
     end
     
     
